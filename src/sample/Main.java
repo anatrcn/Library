2a92dc7c3f3780d579b728350fcbcdb2d76d07b4
Ana <claudiu.haidu@p3-holding.com>
2021-05-12 10:00:38 +0300

    added nitrite library + added one user

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,11 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.dizitart.no2.Document;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.NitriteCollection;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.dizitart.no2.Document.createDocument;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login/login.fxml"));
         primaryStage.setTitle("Book-borrowing");
         primaryStage.setScene(new Scene(root, 600, 550));
@@ -18,6 +26,20 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
+        Nitrite db = Nitrite.builder()
+                .compressed()
+                .filePath("db/book.db")
+                .openOrCreate("user", "password");
+
+        // Create a Nitrite Collection
+        NitriteCollection collection = db.getCollection("users");
+        Document doc = createDocument("username", "Ana")
+                .put("password", "password");
+
+        // insert the document
+        collection.insert(doc);
+
         launch(args);
     }
 }

21f6f7677a31513390f3e3e3eb17a212feaff075
Ana <claudiu.haidu@p3-holding.com>
2021-05-13 17:49:42 +0300

    check if passwords match

--- src/sample/register/RegisterController.java
+++ src/sample/register/RegisterController.java
@@ -1,4 +1,32 @@
 package sample.register;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
 public class RegisterController {
+
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    PasswordField repeatPasswordField;
+    @FXML
+    Label passwordLabel;
+    @FXML
+    Label repeatPasswordLabel;
+
+    public void registerUser(ActionEvent actionEvent) {
+        if (passwordField.getText().equals(repeatPasswordField.getText())) {
+            passwordLabel.setText("");
+            repeatPasswordLabel.setText("");
+        } else {
+            passwordLabel.setText("Password does not match");
+            repeatPasswordLabel.setText("Repeat Password does not match");
+        }
+    }
 }

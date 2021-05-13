ec56b3b65e1284248e7fa370838ed9efb661cfe4
Ana <claudiu.haidu@p3-holding.com>
2021-05-13 16:56:10 +0300

    go to register window method implemented

--- src/sample/login/LoginController.java
+++ src/sample/login/LoginController.java
@@ -1,9 +1,33 @@
 package sample.login;
 
+import com.sun.javafx.css.Style;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class LoginController {
     public void goToRegisterScreen(ActionEvent actionEvent) {
-        System.out.println("Test");
+        ((Node) actionEvent.getSource()).getScene().getWindow().hide();
+        try {
+            loadWindow("../register/register.fxml", "Registration");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void loadWindow(String location, String title) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(location));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage(StageStyle.DECORATED);
+        stage.setScene(scene);
+        stage.setTitle(title);
+        stage.show();
     }
 }
